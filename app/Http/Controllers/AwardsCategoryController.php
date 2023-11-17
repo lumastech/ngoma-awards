02c96626628199e2475d4ca5bdb70ef2f00a09e7
Lumas <lumastech@gmail.com>
2023-11-17 12:06:31 +0200

    Changes to be committed: 	new file:   app/Http/Controllers/ArtistController.php 	new file:   app/Http/Controllers/AwardController.php 	new file:   app/Http/Controllers/AwardsCategoryController.php

---
+++ app/Http/Controllers/AwardsCategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AwardsCategory;
+use Illuminate\Http\Request;
+
+class AwardsCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AwardsCategory $awardsCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AwardsCategory $awardsCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AwardsCategory $awardsCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AwardsCategory $awardsCategory)
+    {
+        //
+    }
+}
