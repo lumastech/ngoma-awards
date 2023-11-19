a145920f9cea10ad6c512a0b00345d2e40608c14
Lumas <lumastech@gmail.com>
2023-11-19 08:16:19 +0200

    updated the dashboard

--- resources/js/Layouts/DashboardLayout.vue
+++ resources/js/Layouts/DashboardLayout.vue
@@ -3,33 +3,33 @@
         <div class="md:flex h-full bg-sky-50">
             <aside @click="sideToggle" class="z-50 md:w-[60px] xl:w-[300px] shrink-0 bg-primary-50 py-7 h-full md:m-2 rounded-md overflow-x-hidden overflow-y-auto absolute md:static md:translate-x-0 -translate-x-full transition">
                 <nav class="flex flex-col px-2 md:px-2">
-                    <a href="/dashboard" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-md my-1 hover:bg-primary-500 hover:text-white transition">
+                    <Link href="/dashboard" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-md my-1 hover:bg-primary-500 hover:text-white transition">
                         <i class="fa-solid fa-home w-7 mr-3"></i>
                         Dashboard
-                    </a>
-                    <a href="/users" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-sm my-1 hover:bg-primary-500 hover:text-white transition">
+                    </Link>
+                    <Link href="/users" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-sm my-1 hover:bg-primary-500 hover:text-white transition">
                         <i class="fa-solid fa-user w-7 mr-3"></i>
                         Users
-                    </a>
-                    <a href="/artists" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-sm my-1 hover:bg-primary-500 hover:text-white transition">
+                    </Link>
+                    <Link href="/artists" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-sm my-1 hover:bg-primary-500 hover:text-white transition">
                         <i class="fa-solid fa-ticket w-7 mr-3"></i>
                         Artists
-                    </a>
-                    <a href="/awards" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-sm my-1 hover:bg-primary-500 hover:text-white transition">
+                    </Link>
+                    <Link href="/awards" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-sm my-1 hover:bg-primary-500 hover:text-white transition">
                         <i class="fa-solid fa-ticket w-7 mr-3"></i>
                         Awards
-                    </a>
-                    <a href="/categories" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-sm my-1 hover:bg-primary-500 hover:text-white transition">
+                    </Link>
+                    <Link href="/categories" class="flex flex-nowrap place-items-center overflow-hidden text-primary-500 px-4 py-2 rounded md:rounded-lg bg-white shadow-sm my-1 hover:bg-primary-500 hover:text-white transition">
                         <i class="fa-solid fa-file-invoice-dollar w-7 mr-3"></i>
                         Award Categories
-                    </a>
+                    </Link>
                 </nav>
             </aside>
             <main class="flex-auto h-full text-gray-500">
                 <div class="flex justify-between p-4 mb-4 place-items-center border-b">
                     <Link href="/dashboard" class="text-gray-500">DASHBOARD</Link>
                     <div class="flex space-x-4">
-                        <Link href="/" class="rounded-full p-2 bg-primary-50 border border-primary-400 w-10 h-10 flex place-items-center justify-center text-primary-500 ">
+                        <Link :href="route('profile.show')" class="rounded-full p-2 bg-primary-50 border border-primary-400 w-10 h-10 flex place-items-center justify-center text-primary-500 ">
                             <i class="fa-solid fa-user"></i>
                         </Link>
                         <Link href="/logout" as="button" type="button" method="post" class="rounded-md p-2 bg-primary-50 border border-primary-400  h-10 flex gap-2 place-items-center justify-center text-primary-500 hover:bg-primary-500 hover:text-white transition ">
