559e5a70224e721da37c8369a8a8c1af2c68bfb0
Trust Chocha <72886735+atombreak@users.noreply.github.com>
2023-11-20 06:04:42 +0200

    Add payment verification logic in VerifyPayment command

--- app/Console/Commands/VerifyPayment.php
+++ app/Console/Commands/VerifyPayment.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 
 class VerifyPayment extends Command
 {
@@ -25,7 +26,35 @@ class VerifyPayment extends Command
      */
     public function handle()
     {
-        //
+        $this->info('Verifying voter\'s payment...');
+
+        $payments = \App\Models\VoterPayment::where('is_verified', false)->get();
+
+            $MSISDN = '';
+            $SESSION_ID = now()->timestamp;
+            $externalID = now()->timestamp;
+            $amount = 2.00;
+            $currency = "ZMW";
+            $token = env('MOMO_TOKEN');
+
+        foreach ($payments as $payment) {
+            // Perform actions for users with balances less than 50
+            // For example, send notifications, update the balance, etc.
+            // You can use $user->id, $user->name, etc. to access user attributes
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $token,
+                'Content-Type' => 'application/json',
+            ])->post('https://lipila-prod.hobbiton.app/transactions/status?transactionId=' . $payment->tnx_id, [
+                'currency' => $currency,
+                'amount' => $amount,
+                'accountNumber' => $MSISDN,
+                'fullName' => "Zipezemo Participant-{$MSISDN}",
+                'phoneNumber' => $MSISDN,
+                'email' => 'user@gmail.com',
+                'externalId' => now()->timestamp,
+                'narration' => 'Ngoma Awards Voting',
+            ]);
+        }
 
         $this->info('Payment verification completed.');
     }
