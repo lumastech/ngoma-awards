9272dc6809b1bb488e3f163534a3c558bdcabada
Trust Chocha <72886735+atombreak@users.noreply.github.com>
2023-11-23 18:46:14 +0200

    Final commit

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -221,12 +221,6 @@ class UssdController extends Controller
 
                 //dd($artist);
 
-                $data = [
-                    'MSISDN' => $MSISDN,
-                    'artist_id' => $artist->id
-                ];
-
-                SendPinPromptEvent::dispatch($data);
 
                 //event(new \App\Events\SendPinPromptEvent($data));
 
@@ -275,6 +269,13 @@ class UssdController extends Controller
                 $response_msg = 'Thank you for your vote, you will soon receive a prompt for a pin shortly.';
 
                 UserJourney::where('phone_number', '=', $MSISDN)->delete();
+
+                $data = [
+                    'MSISDN' => $MSISDN,
+                    'artist_id' => $artist->id
+                ];
+
+                SendPinPromptEvent::dispatch($data);
 
                 return response($response_msg, 200)
                     ->header('Freeflow', 'FB')
